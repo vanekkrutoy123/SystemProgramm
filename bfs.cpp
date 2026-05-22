cb707eae794de5e61f2f1342bf270f3fe26d9e9d
vanekkrutoy123 <ivan.caharov2008@gmail.com>
2026-05-22 10:25:50 +0000

    123

--- bfs.cpp
+++ bfs.cpp
@@ -2,8 +2,26 @@
 #include <vector>
 #include <queue>
 
-void( std:: << queue<int> Q string label){
-std::<<cout label
-while(!q.empty)
-
+void printQueue(std:: << queue<int> Q string label){    
+    std::<<cout label
+    while(!q.empty){    
+        std::<<cout q.front() << " " q.pop();
+    }
+}
+class  {    
+    int numVertices;
+    std::vector<std::vector<int>> adj;
+    std::vector<bool>visited;
+    bool verbose;
+    public:
+Graph (int V, bool verboseMode = false) { 
+ numVertices = V;
+ adj.resize(V);
+ visited resize( V, false),
+ verbose = verboseMode;
+        }
+    }   
+    void addEdge(int U, int V){ 
+adj[U].push_back(V);
+adj[V].push_back(U);
 }
