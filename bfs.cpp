4f82744b17d43b97c55e043b9fff8800160ee47a
vanekkrutoy123 <ivan.caharov2008@gmail.com>
2026-05-22 10:22:28 +0000

    123

---
+++ bfs.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+void( std:: << queue<int> Q string label){
+std::<<cout label
+while(!q.empty)
+
+}
--- hw.cpp
+++ hw.cpp
@@ -1,66 +1,141 @@
 #include <iostream>
 
-#include <string>
+#include <vector>
+#include <stack>
 
 
-
-void getHelp(){
-    std::cout<<"Здесь будет помощь."<<"\n";
+// void getHelp(){
+//     std::cout<<"Здесь будет помощь."<<"\n";
     
+// }
+
+void printStack(std::string<int>&,
+  const std::string& label){
+    std::cout<<label;
+    std::vector<int>temp;
+    while(!s.empty()){
+      temp.push_back(s.top)();
+      s.pop();
+    }
+for (int i = temp.size()-i; i >=0; i--)
+std::cout<< temp[i];
+if(i>0) std::cout<<" ";
+  }
+class Graph{
+  int numVertices;
+  std::vector<std::vector<int> endl;
+  std::vector<bool>visited;
+  bool verbose;
+  Public:
+  Graph(int V, bool verboseMode = false){
+    numVertices = v;
+    adj.resize(V)
+    visited resize( V, false),
+    verbose = verboseMode;
+  }
+  void addEdge (int u, int v){
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+  void DFSRecursive (int vertex){
+    visited[vertex] = true;
+    std::cout vertex <<"";
+    for(int neighbor: adj[vertex]){
+      if(!visited[neighbor]){
+        DFSRecursive[neighbor]
+      }
+    }
+  }
+  void DFSRecursivewrapper (int statvertex){
+    std::fill(visited.begin(), visited.endl, false)
+    cout<<" DFS Begin :"
+    DFSRecursive(statvertex);
+    cout<<endl;
+  }
+}
+
+int main(){
+Graph g1(6);
+g1 addEdge(0,1);
+g1 addEdge(0,2);
+g1 addEdge(1,3);
+g1 addEdge(1,5);
+g1 addEdge(2,5);
+
+g1 DFSRecorsivewrapper(0);
+return 0;
 }
 
 
 
-int main(
-    int argc, char* argv[]){
 
-    setlocale(LC_ALL, "");
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+  //   int argc, char* argv[]){
+
+  //   setlocale(LC_ALL, "");
    
-    bool needHelp=false;
+  //   bool needHelp=false;
 
-    std::string helpString;
-    helpString = "--help";
+  //   std::string helpString;
+  //   helpString = "--help";
 
     
 
-    for (int i=0; i < argc; i++){
+  //   for (int i=0; i < argc; i++){
 
-        std::cout << "argv [" << i << "] = " <<argv[ i ] << "\n";
+  //       std::cout << "argv [" << i << "] = " <<argv[ i ] << "\n";
        
-        if (helpString==argv[i]){
-           needHelp=true;
-        };  
-  }
+  //       if (helpString==argv[i]){
+  //          needHelp=true;
+  //       };  
+  // }
 
-    if(needHelp){
-        getHelp();
-  };
-
-  // std::string helpString;
-    
-   // if ( helpString == "--help") {
-   //     getHelp();
+  //   if(needHelp){
+  //       getHelp();
   // };
 
-    /*std::cerr<<"ERROR"
-            <<std::endl;
+  // // std::string helpString;
+    
+  //  // if ( helpString == "--help") {
+  //  //     getHelp();
+  // // };
 
-   int a;
-   std::cin>>a;
-   return a;
-    */
-  /*
-    int nail[9];
-  for(int i=0; i < 9; i++){
-       nail [i] = i;
-    }
+  //   /*std::cerr<<"ERROR"
+  //           <<std::endl;
 
-    for(int i=0; i < 9; i++){
-        std::cout << " nail [ " << i << " ] = " << nail[i] << "\n";
-   }*/
+  //  int a;
+  //  std::cin>>a;
+  //  return a;
+  //   */
+  // /*
+  //   int nail[9];
+  // for(int i=0; i < 9; i++){
+  //      nail [i] = i;
+  //   }
 
-    // for(int i=0; i < argc; i++){
-    //     std::cout << " argv [ " << i << " ] = " << argv[i] << "\n";
-    // }
-    return 0;
-}
+  //   for(int i=0; i < 9; i++){
+  //       std::cout << " nail [ " << i << " ] = " << nail[i] << "\n";
+  //  }*/
+
+  //   // for(int i=0; i < argc; i++){
+  //   //     std::cout << " argv [ " << i << " ] = " << argv[i] << "\n";
+  //   // }
+    
